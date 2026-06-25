ef093d5f08f645b24f3081f2c0234844cd2e494d
Bienvenu <b_makedikamakuala@hetic.eu>
2026-06-26 01:18:12 +0200

    refactor(dbt): standardize customer_unique_id naming in staging layer

--- dbt_project/models/staging/stg_customers.sql
+++ dbt_project/models/staging/stg_customers.sql
@@ -1,6 +1,6 @@
 SELECT
     customer_id,
-    CAST(customer_unique_id AS STRING) AS unique_id,
+    CAST(customer_unique_id AS STRING) AS customer_unique_id,
     CAST(customer_zip_code_prefix AS STRING) AS zip_code,
     CAST(customer_city AS STRING) AS city,
     CAST(customer_state AS STRING) AS state
