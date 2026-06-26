761dace9c865cfb028b11771fd7c23ac46694485
Bienvenu <b_makedikamakuala@hetic.eu>
2026-06-26 19:30:30 +0200

    feat(dbt): add revenue calculation in fct_orders using order items join

--- dbt_project/models/marts/fct_orders.sql
+++ dbt_project/models/marts/fct_orders.sql
@@ -1,7 +1,10 @@
 SELECT
-    DATE(purchase_ts) AS order_date,
-    COUNT(*) AS total_orders
+    DATE(o.purchase_ts) AS order_date,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    SUM(i.price) AS total_revenue
 
-FROM {{ ref('stg_orders') }}
+FROM {{ ref('stg_orders') }} o
+JOIN {{ ref('stg_order_items') }} i
+ON o.order_id = i.order_id
 
 GROUP BY order_date
