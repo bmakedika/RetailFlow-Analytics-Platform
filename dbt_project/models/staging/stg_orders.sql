6eb5a026d12dc2a153a7d363623b3f1fad4fecb6
Bienvenu <b_makedikamakuala@hetic.eu>
2026-07-05 00:17:53 +0200

    style(dbt): remove trailing comma from stg_orders model

--- dbt_project/models/staging/stg_orders.sql
+++ dbt_project/models/staging/stg_orders.sql
@@ -3,6 +3,6 @@ SELECT
     customer_id,
     order_status,
     CAST(order_purchase_timestamp AS TIMESTAMP) AS purchase_ts,
-    CAST(order_delivered_customer_date AS TIMESTAMP) AS delivered_ts,
+    CAST(order_delivered_customer_date AS TIMESTAMP) AS delivered_ts
 
 FROM `retailflow-analytics.ecommerce_raw.orders`
